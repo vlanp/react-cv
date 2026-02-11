e4acf7f7f2abfe723e954c55747d5c404e1943f9
Valentin GUILLAUME <guillaume.valentin76@gmail.com>
2026-02-11 17:25:14 +0100

    Add a button to hide the panel

--- src/components/item-selector-panel.tsx
+++ src/components/item-selector-panel.tsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import type { IItemSelectorGroup } from "../types/IItemSelectorGroup";
 import ItemSelectorGroup from "./item-selector-group";
 import "./item-selector-panel.css";
+import { FaSquareCaretDown } from "react-icons/fa6";
+import { FaSquareCaretUp } from "react-icons/fa6";
 
 const ItemSelectorPanel = ({
   itemSelectorGroupsProps,
 }: {
   itemSelectorGroupsProps: IItemSelectorGroup[];
 }) => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShow((pv) => !pv);
+  };
+
   return (
-    <div className="item-selector-panel">
-      <h1 className="item-selector-panel-title">RiRi</h1>
-      <div className="item-selector-groups">
-        {itemSelectorGroupsProps.map((itemSelectorGroupProps) => (
-          <ItemSelectorGroup itemSelectorGroupProps={itemSelectorGroupProps} />
-        ))}
+    <>
+      <div className={"item-selector-panel" + (show ? "" : " hide")}>
+        <h1 className="item-selector-panel-title">RiRi</h1>
+        <div className="item-selector-groups">
+          {itemSelectorGroupsProps.map((itemSelectorGroupProps) => (
+            <ItemSelectorGroup
+              itemSelectorGroupProps={itemSelectorGroupProps}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <p
+        className="item-selector small-size-text normal-color-text text-with-icon hover"
+        onClick={handleClick}
+      >
+        {show ? (
+          <FaSquareCaretDown className="large-icon" />
+        ) : (
+          <FaSquareCaretUp className="large-icon" />
+        )}
+      </p>
+    </>
   );
 };
 
